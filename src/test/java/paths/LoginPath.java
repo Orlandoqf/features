efdb3c419f4e74101dff2352cd2d8dc386429b6a
Juan <juanrebatta@bcp.com.pe>
2021-12-16 00:44:12 -0500

    chromedriver en gitignore

--- src/test/java/paths/LoginPath.java
+++ src/test/java/paths/LoginPath.java
@@ -9,5 +9,4 @@ public class LoginPath {
     public By btnLogin = By.name("submit");
     public By txtMensajeError = By.xpath("//span");
     public String url = "http://demo.guru99.com/test/newtours/index.php";
-    public By test = By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[2]/td[3]/form/table/tbody/tr[4]/td/table/tbody/tr[2]/td[1]/font");
 }
